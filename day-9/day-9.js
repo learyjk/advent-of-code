dea722073769c444b4e75e6f0a9a22173425d1ed
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2022-12-09 10:46:38 -0800

    day 9 part 2

--- day-9/day-9.js
+++ day-9/day-9.js
@@ -8,8 +8,8 @@ let headPositions = [];
 let tailPositions = [];
 headPositions.push({ x: 0, y: 0 });
 tailPositions.push({ x: 0, y: 0 });
-let getNextHeadPosition = (direction) => {
-    let { x, y } = headPositions[headPositions.length - 1];
+let getNextHeadPosition = (direction, positions) => {
+    let { x, y } = positions[positions.length - 1];
     if (direction === "R") {
         x++;
     }
@@ -78,7 +78,7 @@ for (const line of lines) {
     let direction = line[0];
     let distance = parseInt(line.slice(2), 10);
     for (let i = 0; i < distance; i++) {
-        let nextHeadPos = getNextHeadPosition(direction);
+        let nextHeadPos = getNextHeadPosition(direction, headPositions);
         let nextTailPos = getNextTailPosition(nextHeadPos);
         headPositions.push(nextHeadPos);
         tailPositions.push(nextTailPos);
@@ -90,4 +90,90 @@ for (const tailPos of tailPositions) {
     tailSet.add(tailPosStr);
 }
 console.log(tailSet.size);
+const knotMap = new Map();
+for (let i = 0; i < 10; i++) {
+    knotMap.set(i, [{ x: 0, y: 0 }]);
+}
+const getNextKnotPosition = (currentKnot, knotInFront) => {
+    let { x: tailX, y: tailY } = currentKnot[currentKnot.length - 1];
+    let { x: headX, y: headY } = knotInFront[knotInFront.length - 1];
+    let deltaX = headX - tailX;
+    let deltaY = headY - tailY;
+    // check if head is more than 1 away from tail
+    if (Math.abs(deltaX) <= 1 && Math.abs(deltaY) <= 1) {
+        // if not then no need to move tail.
+        return { x: tailX, y: tailY };
+    }
+    // if head is more than 1 away from tail then move tail towards head
+    if (deltaX > 1) {
+        tailX++;
+        // diagonal chance
+        if (deltaY > 0) {
+            tailY++;
+        }
+        if (deltaY < 0) {
+            tailY--;
+        }
+        return { x: tailX, y: tailY };
+    }
+    if (deltaX < -1) {
+        tailX--;
+        if (deltaY > 0) {
+            tailY++;
+        }
+        if (deltaY < 0) {
+            tailY--;
+        }
+        return { x: tailX, y: tailY };
+    }
+    if (deltaY > 1) {
+        tailY++;
+        if (deltaX > 0) {
+            tailX++;
+        }
+        if (deltaX < 0) {
+            tailX--;
+        }
+        return { x: tailX, y: tailY };
+    }
+    if (deltaY < -1) {
+        tailY--;
+        if (deltaX > 0) {
+            tailX++;
+        }
+        if (deltaX < 0) {
+            tailX--;
+        }
+        return { x: tailX, y: tailY };
+    }
+    return { x: tailX, y: tailY };
+};
+for (const line of lines) {
+    let direction = line[0];
+    let distance = parseInt(line.slice(2), 10);
+    for (let i = 0; i < distance; i++) {
+        console.log(`move head ${direction} ${i}/${distance}`);
+        for (let k = 0; k < knotMap.size; k++) {
+            let currentKnot = knotMap.get(k);
+            if (k === 0) {
+                let nextPos = getNextHeadPosition(direction, currentKnot);
+                console.log(`knot ${k} nextPos ${JSON.stringify(nextPos)}`);
+                currentKnot.push(nextPos);
+            }
+            else {
+                let knotInFront = knotMap.get(k - 1);
+                let nextPos = getNextKnotPosition(currentKnot, knotInFront);
+                console.log(`knot ${k} nextPos ${JSON.stringify(nextPos)}`);
+                currentKnot.push(nextPos);
+            }
+        }
+    }
+}
+console.log(knotMap.get(9));
+const tailSetTwo = new Set();
+for (const pos of knotMap.get(9)) {
+    const posStr = JSON.stringify(pos);
+    tailSetTwo.add(posStr);
+}
+console.log(tailSetTwo.size);
 //# sourceMappingURL=day-9.js.map
--- day-9/day-9.ts
+++ day-9/day-9.ts
@@ -16,8 +16,8 @@ let tailPositions: Point[] = [];
 headPositions.push({ x: 0, y: 0 });
 tailPositions.push({ x: 0, y: 0 });
 
-let getNextHeadPosition = (direction: string): Point => {
-  let { x, y } = headPositions[headPositions.length - 1];
+let getNextHeadPosition = (direction: string, positions: Point[]): Point => {
+  let { x, y } = positions[positions.length - 1];
   if (direction === "R") {
     x++;
   } else if (direction === "L") {
@@ -88,7 +88,7 @@ for (const line of lines) {
   let direction = line[0];
   let distance = parseInt(line.slice(2), 10);
   for (let i = 0; i < distance; i++) {
-    let nextHeadPos = getNextHeadPosition(direction);
+    let nextHeadPos = getNextHeadPosition(direction, headPositions);
     let nextTailPos = getNextTailPosition(nextHeadPos);
     headPositions.push(nextHeadPos);
     tailPositions.push(nextTailPos);
@@ -102,3 +102,99 @@ for (const tailPos of tailPositions) {
 }
 
 console.log(tailSet.size);
+
+const knotMap = new Map<number, Point[]>();
+for (let i = 0; i < 10; i++) {
+  knotMap.set(i, [{ x: 0, y: 0 }]);
+}
+
+const getNextKnotPosition = (
+  currentKnot: Point[],
+  knotInFront: Point[]
+): Point => {
+  let { x: tailX, y: tailY } = currentKnot[currentKnot.length - 1];
+  let { x: headX, y: headY } = knotInFront[knotInFront.length - 1];
+
+  let deltaX = headX - tailX;
+  let deltaY = headY - tailY;
+
+  // check if head is more than 1 away from tail
+  if (Math.abs(deltaX) <= 1 && Math.abs(deltaY) <= 1) {
+    // if not then no need to move tail.
+    return { x: tailX, y: tailY };
+  }
+
+  // if head is more than 1 away from tail then move tail towards head
+  if (deltaX > 1) {
+    tailX++;
+    // diagonal chance
+    if (deltaY > 0) {
+      tailY++;
+    }
+    if (deltaY < 0) {
+      tailY--;
+    }
+    return { x: tailX, y: tailY };
+  }
+  if (deltaX < -1) {
+    tailX--;
+    if (deltaY > 0) {
+      tailY++;
+    }
+    if (deltaY < 0) {
+      tailY--;
+    }
+    return { x: tailX, y: tailY };
+  }
+  if (deltaY > 1) {
+    tailY++;
+    if (deltaX > 0) {
+      tailX++;
+    }
+    if (deltaX < 0) {
+      tailX--;
+    }
+    return { x: tailX, y: tailY };
+  }
+  if (deltaY < -1) {
+    tailY--;
+    if (deltaX > 0) {
+      tailX++;
+    }
+    if (deltaX < 0) {
+      tailX--;
+    }
+    return { x: tailX, y: tailY };
+  }
+  return { x: tailX, y: tailY };
+};
+
+for (const line of lines) {
+  let direction = line[0];
+  let distance = parseInt(line.slice(2), 10);
+  for (let i = 0; i < distance; i++) {
+    console.log(`move head ${direction} ${i}/${distance}`);
+    for (let k = 0; k < knotMap.size; k++) {
+      let currentKnot = knotMap.get(k);
+      if (k === 0) {
+        let nextPos = getNextHeadPosition(direction, currentKnot);
+        console.log(`knot ${k} nextPos ${JSON.stringify(nextPos)}`);
+        currentKnot.push(nextPos);
+      } else {
+        let knotInFront = knotMap.get(k - 1);
+        let nextPos = getNextKnotPosition(currentKnot, knotInFront);
+        console.log(`knot ${k} nextPos ${JSON.stringify(nextPos)}`);
+        currentKnot.push(nextPos);
+      }
+    }
+  }
+}
+
+console.log(knotMap.get(9));
+
+const tailSetTwo: Set<string> = new Set();
+for (const pos of knotMap.get(9)) {
+  const posStr = JSON.stringify(pos);
+  tailSetTwo.add(posStr);
+}
+console.log(tailSetTwo.size);
